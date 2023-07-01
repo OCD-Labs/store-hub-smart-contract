1d152da73da281958b95b3a74bcd68a4714fd073
IkehAkinyemi <mrikehchukwuka@gmail.com>
2023-07-01 08:45:28 +0100

    setup the initial-expected state of smart contract

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,31 @@
-use near_sdk::collections::{LookupMap, TreeMap, UnorderedSet};
-use near_sdk::{self, near_bindgen, AccountId, PanicOnDefault};
+use near_sdk::collections::{LookupMap, UnorderedSet, TreeMap, UnorderedMap};
+use near_sdk::{self, near_bindgen, PanicOnDefault, AccountId};
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 
+//every transaction will have a unique ID which is `STOREID + DELIMITER + ITEM_ID`
+static DELIMETER: &str = ".";
+
+//Creating custom types to use within the contract. This makes things more readable.
 pub type ItemId = String;
-pub type StoreId = String;
+pub type StoreId = AccountId;
+pub type StoreIdAndItemId = String;
 
 pub struct ItemMetadata {
     pub name: String,
     pub price: String,
-    pub img_url: String,
+    pub img_url: String
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
-
 pub struct Contract {
     pub overseer_id: AccountId,
     pub stores_per_account_id: Option<LookupMap<AccountId, UnorderedSet<StoreId>>>,
     pub item_by_store_id: TreeMap<ItemId, StoreId>,
-    pub item_metadata_by_id: Option<LookupMap<ItemId, ItemMetadata>>,
+    pub transactions: Option<UnorderedMap<StoreIdAndItemId, ItemMetadata>>
 }
 
-impl Contract {}
+impl Contract {
+    
+}
+
